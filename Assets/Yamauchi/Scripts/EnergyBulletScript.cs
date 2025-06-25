822cef82c7f2703d7fd3866efa82a093133a4901
Ubara0715 <c24028@std.it-college.ac.jp>
2025-06-25 15:50:12 +0900

    整えてみた

--- Assets/Yamauchi/Scripts/EnergyBulletScript.cs
+++ Assets/Yamauchi/Scripts/EnergyBulletScript.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class EnergyBulletScript : MonoBehaviour
 {
-    // privert関数
-
     // 距離減衰をリアルタイムで減少させたい
+    // パーティクルは生成して残像残す
 
-    float duration = 0;
+    // ダメージ計算
+    float range = 0;
+    int damege = 0;
+    [HideInInspector] public float usedEnergy_clone;
+    [HideInInspector] public float speed_clone;
 
+    // 距離計算
     Vector3 startPos;
     float distance = 0;
 
-    [HideInInspector] public float usedEnergy;
-
+    // カラー変更
     Color color;
     ParticleSystem ps;
 
+    // メソッド
     void Awake()
     {
         ps = GetComponent<ParticleSystem>();
@@ -25,24 +29,9 @@ public class EnergyBulletScript : MonoBehaviour
         startPos = transform.position;
     }
 
-    public int EnergyDamege()
-    {
-        if ((int)(distance / 100) >= 1)
-        {
-            for (int _ = 0; _ <= (int)(distance / 100); _++)
-            {
-                DistanceDecay();
-            }
-        }
-
-        return (int)usedEnergy;
-    }
-
     void Update()
     {
-        duration += Time.deltaTime;
-
-        if(usedEnergy <= 0)
+        if(usedEnergy_clone <= 0)
         {
             Destroy(gameObject);
         }
@@ -60,10 +49,34 @@ public class EnergyBulletScript : MonoBehaviour
         //Debug.Log(EnergyDamege());
     }
 
+    // 関数
+
+    // ダメージ算出
+    public int EnergyDamege()
+    {
+        if ((int)(distance / 100) >= 1)
+        {
+            for (int _ = 0; _ <= (int)(distance / 100); _++)
+            {
+                DistanceDecay();
+            }
+        }
+
+        damege = (int)usedEnergy_clone;
+
+        return damege;
+    }
+
     // 距離減衰用の関数、動くのは確認済み
     void DistanceDecay()
     {
-        usedEnergy = (int)(usedEnergy * 0.8f);
+        usedEnergy_clone = (int)(usedEnergy_clone * 0.8f);
         //Debug.Log("ダメージ減少！");
+    }
+
+    void FilghtDistance()
+    {
+        range += speed_clone / Time.fixedDeltaTime;
+        Invoke("FilghtDistance", Time.fixedDeltaTime);
     }
 }
